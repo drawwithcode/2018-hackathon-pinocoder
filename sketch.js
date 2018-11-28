5f9b0c96befa7c29af8f4084b6c2c17a047cf419
pinocoder <41514345+pinocoder@users.noreply.github.com>
2018-11-28 12:26:51 +0100

    hackaton_pino_1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,61 @@
-function preload(){
-  // put preload code here
+var song;
+var img;
+var analyser;
+
+function preload() {
+  song = loadSound('assets/goodbye.mp3');
+  img = loadImage("assets/dark_hackaton.jpg");
 }
+
+// ————————————————————————————————————————————————————————————————————————————————————————————————
+
 
 function setup() {
-  // put setup code here
+
+  createCanvas(windowWidth, windowHeight);
+
+
+  song.loop();
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(song);
+
+
+
 }
 
+// ————————————————————————————————————————————————————————————————————————————————————————————————
+
 function draw() {
-  // put drawing code here
+
+  //CREATE A BACKGROUND ALSO WITH AN IMG
+  background(111, 113, 145);
+  imageMode(CENTER);
+  image(img, windowWidth / 2, windowHeight / 2, width / 2, height / 2);
+
+
+  //CREATE A PULSATING POINT
+  volume = analyzer.getLevel();
+  console.log(volume);
+  volume = map(volume,0,0.5,10,200);
+  noStroke();
+  fill(255, 255, 0);
+  ellipse(windowWidth / 2, windowHeight / 1.5 + 60, volume);
+
+
+  textFont('Karla');
+  fill(255, 255, 0)
+  textSize(24);
+  text('DARK — by Baran bo Odar and Jantje Friese', 50, 100);
+
+  // mySong.amp(0.5);
+  // mySong.rate(2);
+
+
+}
+
+// ————————————————————————————————————————————————————————————————————————————————————————————————
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
